b1fa65f21904c1eb7d112b82dd4edeb18e3a6622
Sudhanshu Siddh <sudhanshu.15@gmail.com>
2017-12-02 18:31:45 -0500

    Added API keys and ran sample app

--- app/src/main/java/com/p532/arbusschedules/ARMainActivity.java
+++ app/src/main/java/com/p532/arbusschedules/ARMainActivity.java
@@ -1,13 +1,68 @@
 package com.p532.arbusschedules;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class ARMainActivity extends AppCompatActivity {
+import eu.kudan.kudan.ARActivity;
+
+public class ARMainActivity extends ARActivity {
+
+
+    private static final int REQUEST_CAMERA = 0;
+    public static final String TAG = "ARMainActivity";
+
+    boolean firstRun = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_armain);
+//        setContentView(R.layout.activity_armain);
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Camera permission has not been granted.
+
+            requestCameraPermission();
+
+        } else {
+
+            // Camera permissions is already available, show the camera preview.
+            Log.i(TAG,
+                    "CAMERA permission has already been granted. Displaying camera preview.");
+        }
+
+    }
+
+    private void requestCameraPermission() {
+        Log.i(TAG, "CAMERA permission has NOT been granted. Requesting permission.");
+
+        // BEGIN_INCLUDE(camera_permission_request)
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.CAMERA)) {
+            // Provide an additional rationale to the user if the permission was not granted
+            // and the user would benefit from additional context for the use of the permission.
+            // For example if the user has previously denied the permission.
+            Log.i(TAG,
+                    "Displaying camera permission rationale to provide additional context.");
+            ActivityCompat.requestPermissions(ARMainActivity.this,
+                    new String[]{Manifest.permission.CAMERA},
+                    REQUEST_CAMERA);
+        } else {
+
+            // Camera permission has not been granted yet. Request it directly.
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                    REQUEST_CAMERA);
+        }
+        // END_INCLUDE(camera_permission_request)
+    }
+
+    @Override
+    public void setup() {
+        super.setup();
     }
 }
