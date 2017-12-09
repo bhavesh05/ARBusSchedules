4071f27af11f29805703f77b10c4358fc4d65425
Sudhanshu Siddh <ssiddh@iu.edu>
2017-12-08 19:58:10 -0500

    Added code to detect the touch on Model Node and launching lyft, also added a new marker for the image marker

--- app/src/main/java/com/p532/arbusschedules/ARMainActivity.java
+++ app/src/main/java/com/p532/arbusschedules/ARMainActivity.java
@@ -1,14 +1,20 @@
 package com.p532.arbusschedules;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Rect;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.util.TypedValue;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.RelativeLayout;
 
@@ -29,14 +35,17 @@ import eu.kudan.kudan.ARMeshNode;
 import eu.kudan.kudan.ARModelImporter;
 import eu.kudan.kudan.ARModelNode;
 import eu.kudan.kudan.ARTexture2D;
+import eu.kudan.kudan.ARView;
 
-public class ARMainActivity extends ARActivity {
+public class ARMainActivity extends ARActivity implements GestureDetector.OnGestureListener{
 
     public static final String TAG = "ARMainActivity";
     private RecyclerView recyclerView;
     private BusDetailAdapter busDetailAdapter;
     private List<BusDetail> busDetailList;
     private LocationManager locationManager;
+    private static boolean ROTATE = false;
+    private GestureDetectorCompat gestureDetect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,33 +68,25 @@ public class ARMainActivity extends ARActivity {
 
         final Map<String,String> timeMap = new HashMap<>();
         BusDataRetrieval busDataRetrieval = new BusDataRetrieval(this,locationManager,timeMap, busDetailList, busDetailAdapter);
-
         busDataRetrieval.getBusData();
-        Log.d(TAG, "onCreate: " + timeMap);
 
-        final android.os.Handler handler = new android.os.Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Log.d(TAG, "run: " + timeMap);
-            }
-        }, 10000);
-
+        gestureDetect = new GestureDetectorCompat(this,this);
     }
     @Override
     public void setup() {
         super.setup();
         ARImageTrackable arImageTrackable = new ARImageTrackable("Bloomington Transit");
-        arImageTrackable.loadFromAsset("mobilebus.jpg");
+        arImageTrackable.loadFromAsset("ARrival_Marker.jpg");
 
         ARImageTracker arImageTracker = ARImageTracker.getInstance();
         arImageTracker.initialise();
         ARModelNode modelNode = prepareModel();
-        final ARGyroPlaceManager gyroPlaceManager = ARGyroPlaceManager.getInstance();
+        ARGyroPlaceManager gyroPlaceManager = ARGyroPlaceManager.getInstance();
         gyroPlaceManager.initialise();
         gyroPlaceManager.getWorld().addChild(modelNode);
         gyroPlaceManager.getWorld().findChildByName("Car").setPosition(0.0f, -100f, 0.0f);
         gyroPlaceManager.getWorld().findChildByName("Car").setVisible(false);
+        rotateModel();
 
         arImageTracker.addTrackable(arImageTrackable);
 
@@ -99,6 +100,7 @@ public class ARMainActivity extends ARActivity {
                         relativeLayout.setVisibility(View.VISIBLE);
                     }
                 });
+                makeModelVisible(false);
             }
 
             @Override
@@ -115,9 +117,53 @@ public class ARMainActivity extends ARActivity {
                         relativeLayout.setVisibility(View.GONE);
                     }
                 });
-                gyroPlaceManager.getWorld().findChildByName("Car").setVisible(true);
+                makeModelVisible(true);
             }
         });
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        gestureDetect.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent motionEvent) {
+        ARView arView = this.getARView();
+        ARGyroPlaceManager arGyroPlaceManager = ARGyroPlaceManager.getInstance();
+        ARModelNode arModelNode = (ARModelNode) arGyroPlaceManager.getWorld().findChildByName("Car");
+
+        if(ARHelper.isNodeSelected(arView, arModelNode, motionEvent, 300)){
+            Log.d(TAG, "onSingleTapUp: Object Clicked" );
+            launchLyft();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        return false;
     }
 
     private int dpToPx(int dp) {
@@ -143,5 +189,53 @@ public class ARMainActivity extends ARActivity {
 
         modelNode.scaleByUniform(2.25f);
         return modelNode;
+    }
+
+    private void launchLyft(){
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+        alertBuilder.setTitle("Get Lyft Instead?");
+        alertBuilder.setMessage("Tired of waiting? Want to get a Lyft instead?");
+        alertBuilder.setNeutralButton("Get Me a Lyft", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+                Intent lyftIntent = getPackageManager().getLaunchIntentForPackage("me.lyft.android");
+                startActivity(lyftIntent);
+            }
+        });
+        alertBuilder.setNegativeButton("No Thanks", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+        AlertDialog launchLyft = alertBuilder.create();
+        launchLyft.show();
+    }
+
+    private void rotateModel(){
+        new Thread(new Runnable() {
+
+            ARGyroPlaceManager gyroPlaceManager = ARGyroPlaceManager.getInstance();
+            @Override
+            public void run() {
+                while(true){
+                    if(ROTATE) {
+                        gyroPlaceManager.getWorld().findChildByName("Car").rotateByDegrees(1, 0, 1, 0);
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }).start();
+    }
+
+    private void makeModelVisible(boolean modelVisible){
+        ARGyroPlaceManager gyroPlaceManager = ARGyroPlaceManager.getInstance();
+        gyroPlaceManager.getWorld().findChildByName("Car").setVisible(modelVisible);
+        ROTATE = modelVisible;
     }
 }
